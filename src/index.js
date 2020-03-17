325d0917c6a8877d4c30023dffc35a29d45ddfeb
Luka Salevic <salevic@lu-ka.me>
2020-03-17 22:06:07 +0100

    Remove redundant 'normalize' import and import placeholder website logo
    Add placeholder logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Imports
-import './styles/normalize.css';
 import './styles/index.scss';
 import './images/linkedin-icon.svg';
 import './images/email-icon.svg';
 import './images/phone-icon.svg';
+import './images/logo.svg';
